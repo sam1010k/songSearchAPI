9cab1bcd23285845be72e7ab9d994b4ccbb6ca19
gitSCSdotcom <samk@getscs.com>
2021-10-03 11:17:38 -0700

    cleaned up code. accessing details through shazams get-details, minimized audioDB. Added documentation

--- pages.py
+++ pages.py
@@ -1,33 +1,53 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, render_template, request
+import time
 import songDetail as SD
 
+# Instance of Flask Class, argument is the name of the application
+# This is needed so that Flask knows where to look for resources such as templates and static files.
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# getting local time to pass to base.html
+seconds = time.time()
+local_time = time.ctime(seconds)
 
-@app.route("/song_search", methods = ["POST","GET"])
-def song_search():
-    if request.method=="POST":
+# Route tells Flask what URL should trigger our function and what kinds of requests we can make
+# home() function returns the HTML template we want to display in the user’s browser... 
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+
+        # grabs value from base.html template form on POST
         song = request.form["sng"]
 
-        shazam_api_query = {"term": song,"locale":"en-US","offset":"0","limit":"5"}
+        # forms query for our API call
+        shazam_api_query = {"term": song,"locale": "en-US", "offset": "0", "limit": "5"}
+
+        # create instance of Song class and pass in query
         songDetail = SD.Song(song, shazam_api_query)
+
+        # responsible for SPI call and assigning all class attributes
         songDetail.set_all_attributes()
-        
-        song_detail_dict = {'Song Searched':songDetail.song , 'Artist': songDetail.artist,
-                            'Album':songDetail.album , 'Description': songDetail.description,
-                            'Genre':songDetail.genre , 'Mood': songDetail.mood,
-                            'Track':songDetail.track , 'Lyrics': songDetail.lyrics}
 
+        # dictionary for each of the attributes to be looped through on base.html
+        song_detail_dict = {'Song Searched': songDetail.song, 'Artist': songDetail.artist,
+                            'Track': songDetail.track, 'Label': songDetail.label,
+                            'Released Date': songDetail.released_date, 'Album': songDetail.album,
+                            'Description': songDetail.description, 'Genre': songDetail.genre,
+                            'Mood': songDetail.mood, 'Lyrics': songDetail.lyrics
+                            }
 
+        # used in base.html to display details or not
         submit = True
-        return render_template("song_search.html", song_detail_dict=song_detail_dict,
-                        albumImg = songDetail.albumImage, youTube = songDetail.youTubeLink,
-                        submit=submit)
+
+        # used to pass values to our base.html template
+        return render_template("base.html", song_detail_dict=song_detail_dict, search_date=local_time,
+                               albumImg=songDetail.albumImage, youTube=songDetail.youtubelink,
+                                thumbnail=songDetail.thumbnail, submit=submit, json=songDetail.shazam_get_details_json_data)
+
+    # otherwise, just render base.html
     else:
-        return render_template("song_search.html")
+        return render_template("base.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- songDetail.py
+++ songDetail.py
@@ -3,67 +3,109 @@ import json
 from requests.exceptions import HTTPError
 from headers import shazam_api_headers, audioDB_api_headers
 
-shazam_api_url = "https://shazam.p.rapidapi.com/search"
+# Urls for the endpoints
+shazam_search_url = "https://shazam.p.rapidapi.com/search"
+shazam_get_details_url = "https://shazam.p.rapidapi.com/songs/get-details"
+audioDB_search_track_url = "https://theaudiodb.p.rapidapi.com/searchtrack.php"
 
-audioDB_api_url = "https://theaudiodb.p.rapidapi.com/searchtrack.php"
+# Song class with attributes or song details
+# Methods that allow us to set the song details though API calls
+
 
 class Song:
 
-    json_data = ''
-
-    # from Shazam
+    # Attributes that come from Shazam
     artist, albumImage = '', ''
+    album, label = '', ''
+    released_date, genre = '', ''
+    youtubelink, lyrics = '', ''
+    song_key, track = '', ''
+    thumbnail = ''
 
-    # from AudioDB
-    album, description = '', ''
-    genre, mood = '', ''
-    youTubeLink, track = '', ''
-    lyrics = ''
+    # Attributes that come from AudioDB
+    description, mood = '', ''
 
-    # Class constructor
-    def __init__(self, song, shazamQuery):
+    # JSON data attributes
+    shazam_search_json_data = ''
+    shazam_get_details_json_data = ''
+    audioDB_get_details_json_data = ''
+
+    # Class constructor, song and query are used in the 1st API call
+    def __init__(self, song, shazamSearchQuery):
         self.song = song
-        self.shazamQuery = shazamQuery
-    
+        self.shazamSearchQuery = shazamSearchQuery
+
     # Sets all of our class attributes, song details
     def set_all_attributes(self):
-
-        # Set data from Shazam API
-        self.json_data = self.get_info_from_api(shazam_api_url, shazam_api_headers, self.shazamQuery)
         try:
-            self.artist = self.json_data["tracks"]["hits"][0]["track"]["subtitle"]
-            self.albumImage = self.json_data["tracks"]["hits"][0]["track"]["share"]["image"]
+            # Shazam Search Query: Get Artist, Album Image, and Song Key
+            self.shazam_search_json_data = self.get_info_from_api(
+                shazam_search_url, shazam_api_headers, self.shazamSearchQuery)
+            shazam_search = self.shazam_search_json_data["tracks"]["hits"][0]["track"]
+
+            # Accesing values from python list, and assigning them to class attributes
+            self.track = shazam_search["title"]
+            self.artist = shazam_search["subtitle"]
+            self.albumImage = shazam_search["share"]["image"]
+            self.song_key = shazam_search["key"]
+
+            # Shazam getDetails Query: Track, Album, Label, Released Date, Lyrics...
+            details_query = {"key": self.song_key, "locale": "en-US"}
+            self.shazam_get_details_json_data = self.get_info_from_api(
+                shazam_get_details_url, shazam_api_headers, details_query)
+            get_details_metadata = self.shazam_get_details_json_data["sections"][0]["metadata"]
+
+            # Accesing values from python list, and assigning them to class attributes
+            self.album = get_details_metadata[0]["text"]
+            self.label = get_details_metadata[1]["text"]
+            self.released_date = get_details_metadata[2]["text"]
+            self.youtubelink = self.shazam_get_details_json_data[
+                "sections"][2]["youtubeurl"]["actions"][0]["uri"]
+            self.thumbnail = self.shazam_get_details_json_data[
+                "sections"][2]["youtubeurl"]["image"]["url"]
+            self.genre = self.shazam_get_details_json_data["genres"]["primary"]
+
+            # Using formatted lyrics method to clean up lyrics
+            self.lyrics = self.formatted_lyrics(
+                self.shazam_get_details_json_data["sections"][1]["text"])
+
         except Exception as err:
-            return 'Cant find the requested song'
+            return f'Error: {err}, occured in Shazam Query'
 
-
-        # Set data from AudioDB
-        audioDB_api_query = {"s": self.artist,"t": self.song}
-        self.json_data = self.get_info_from_api(audioDB_api_url, audioDB_api_headers, audioDB_api_query)
-        self.album = self.get_audioDB_json_string("Album", "strAlbum")
-        self.description = self.get_audioDB_json_string("Description", "strDescriptionEN")
-        self.genre = self.get_audioDB_json_string("Genre", "strGenre")
+        # Set data attributes from AudioDB
+        audioDB_api_query = {"s": self.artist, "t": self.song}
+        self.audioDB_get_details_json_data = self.get_info_from_api(
+            audioDB_search_track_url, audioDB_api_headers, audioDB_api_query)
+        self.description = self.get_audioDB_json_string(
+            "Description", "strDescriptionEN")
         self.mood = self.get_audioDB_json_string("Mood", "strMood")
-        self.youTubeLink = self.get_audioDB_json_string("YouTube Link", "strMusicVid")
-        self.track = self.get_audioDB_json_string("Track", "strTrack")
-        self.lyrics = self.get_audioDB_json_string("Lyrics", "strTrackLyrics")
-    
+
     # Used to query api
-    def get_info_from_api(self,url,header,query):
+    def get_info_from_api(self, url, header, query):
         try:
-            response = requests.request("GET", url, headers=header, params=query)
+            response = requests.request(
+                "GET", url, headers=header, params=query)
             response.raise_for_status()
+            # json.loads returns a python list...
             json_data = json.loads(response.text)
             return json_data
         except HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
         except Exception as err:
             print(f'Other error occurred: {err}')
-    
-    # Error handling when detail not found in database
+
+    # Used to format data from audioDB and...
+    # ...error handling when detail not found in AudioDB
     def get_audioDB_json_string(self, attr, DBstr):
         try:
-            str = self.json_data["track"][0][DBstr]
+            str = self.audioDB_get_details_json_data["track"][0][DBstr]
             return str
         except Exception as err:
             return 'Cant find ' + attr
+
+    # Formats lyrics data into more user readable
+    def formatted_lyrics(self, lyrics_list):
+        formatted = ''
+        for line in lyrics_list:
+            formatted = formatted + line + '\n'
+        return formatted
